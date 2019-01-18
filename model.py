40f684719ef841abedac91cfb8f4e7a0794f5ebf
Iris Han <irisbhan@gmail.com>
2019-01-18 01:31:15 -0800

    Created Models for database.

--- model.py
+++ model.py
@@ -10,12 +10,84 @@ db = SQLAlchemy()
 
 # Model definitions
 
+class Brand(db.Model):
+    """Brand of past/complete projects."""
+
+    __tablename__ = "brands"
+
+    brand_id = db.Column(db.Integer,
+                         autoincrement=True,
+                         primary_key=True)
+    brand_name = db.Column(db.String(50), unique=True, nullable=False)
+    website = db.Column(db.String(50), nullable=True)
+    description_top = db.Column(db.Text, nullable=False)
+    founded_yr = db.Column(db.Integer, nullable=True)
+    title = db.Column(db.String(50), nullable=False)
+    description_bottom = db.Column(db.Text, nullable=True)
+    banner_img = db.Column(db.String(50), nullable=True)
+    photo_a = db.Column(db.String(50), nullable=True)
+    photo_b = db.Column(db.String(50), nullable=True)
+    photo_c = db.Column(db.String(50), nullable=True)
+    photo_d = db.Column(db.String(50), nullable=True)
+
+    def __repr__(self):
+        """Representation when printed."""
+
+        return "<Brand brand_id=%s brand_name=%s>" % (self.brand_id,
+                                                      self.brand_name)
+
+    #Define relationship products table
+    products = db.relationship("Product",
+                               backref=db.backref("brands"))
+
+
+class Product(db.Model):
+    """Product of brands."""
+
+    __tablename__ = "products"
+
+    product_id = db.Column(db.Integer,
+                           autoincrement=True,
+                           primary_key=True)
+    product_name = db.Column(db.String(50), nullable=False)
+    description = db.Column(db.Text, nullable=True)
+    size = db.Column(db.String(30), nullable=True)
+    photo_y = db.Column(db.String(50), nullable=False)
+    photo_z = db.Column(db.String(50), nullable=True)
+    brand_id = db.Column(db.Integer,
+                         db.ForeignKey('brands.brand_id'))
+
+    def __repr__(self):
+        """Representation when printed."""
+
+        return "<Product product_id=%s product_name=%s>" % (self.product_id,
+                                                            self.product_name)
+
+
+class Project(db.Model):
+    """Work in progress."""
+
+    __tablename__ = "projects"
+
+    project_id = db.Column(db.Integer,
+                           autoincrement=True,
+                           primary_key=True)
+    project_name = db.Column(db.String(50), nullable=False)
+    logo_img = db.Column(db.String(50), nullable=False)
+    website = db.Column(db.String(50), nullable=False)
+    photo = db.Column(db.String(50), nullable=False)
+
+    def __repr__(self):
+        """Representation when printed."""
+
+        return "<Project project_id=%s project_name=%s>" % (self.project_id,
+                                                            self.project_name)
 
 
 #####   HELPER FUNCTIONS  ######################################################
 
 def connect_to_db(app, db_uri='postgresql:///company'):
-    """Connect the database to our Flask app."""
+    """Connect the database to the Flask app."""
 
     app.config['SQLALCHEMY_DATABASE_URI'] = db_uri
     db.app = app
@@ -27,4 +99,4 @@ if __name__ == "__main__":
 
     from server import app
     connect_to_db(app)
-    print "Connected to DB."
+    print ("Connected to DB.")
--- server.py
+++ server.py
@@ -6,7 +6,7 @@ from flask import Flask, render_template, redirect, flash, session, request
 from flask_debugtoolbar import DebugToolbarExtension
 from model import connect_to_db, db
 import json
-from werkzeug import secure_filename
+# from werkzeug import secure_filename
 
 app = Flask(__name__)
 app.secret_key = "ABC"
@@ -21,46 +21,46 @@ def index():
     return render_template("index.html")
 
 
-@app.route('/projects')
+@app.route('/brands')
 def show_projects():
-    """List of past projects."""
+    """List of past projects by brand."""
 
-    return render_template("index.html")
+    return render_template("brands.html")
 
 
 @app.route('/upcoming')
 def show_upcoming_projects():
     """List of works in progress."""
 
-    return render_template("index.html")
+    return render_template("upcoming.html")
 
 
 @app.route('/misc')
 def show_misc_projects():
     """List of miscellaneous projects."""
 
-    return render_template("index.html")
+    return render_template("misc.html")
 
 
 @app.route('/about')
 def show_about_us():
     """Description of company."""
 
-    return render_template("index.html")
+    return render_template("about.html")
 
 
 @app.route('/contact')
 def show_contact():
     """Contact information and form."""
 
-    return render_template("index.html")
+    return render_template("contact.html")
 
 
 @app.route('/submit', methods=["POST"])
 def submit_contact_form():
     """Submit contact form."""
 
-    return render_template("index.html")
+    return render_template("submit.html")
 
 
 ######### HELPER FUNCTIONS ####################################################
