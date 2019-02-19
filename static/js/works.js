b558c41b67a73af51b7cefcb8555215dd70e44ed
Iris Han <irisbhan@gmail.com>
2019-02-19 03:33:07 -0800

    Updated Works page. Adjusted alignment. Added JS.

---
+++ static/js/works.js
@@ -0,0 +1,83 @@
+$('.brand-a').on('mouseover', function(evt) {
+  $('.works-content').css('background', '#403079f0');
+});
+$('.brand-a').on('mouseout', function(evt) {
+  $('.works-content').css('background', '#dea2c2f0');
+});
+
+$('.brand-b').on('mouseover', function(evt) {
+  $('.works-content').css('background', '#41c3c0f0');
+});
+$('.brand-b').on('mouseout', function(evt) {
+  $('.works-content').css('background', '#dea2c2f0');
+});
+
+$('.brand-c').on('mouseover', function(evt) {
+  $('.works-content').css('background', '#e67d2bf0');
+});
+$('.brand-c').on('mouseout', function(evt) {
+  $('.works-content').css('background', '#dea2c2f0');
+});
+
+$('.brand-d').on('mouseover', function(evt) {
+  $('.works-content').css('background', '#4e3339f0');
+});
+$('.brand-d').on('mouseout', function(evt) {
+  $('.works-content').css('background', '#dea2c2f0');
+});
+
+$('.brand-e').on('mouseover', function(evt) {
+  $('.works-content').css('background', '#2f2a2df0');
+});
+$('.brand-e').on('mouseout', function(evt) {
+  $('.works-content').css('background', '#dea2c2f0');
+});
+
+$('.brand-f').on('mouseover', function(evt) {
+  $('.works-content').css('background', '#212565');
+});
+$('.brand-f').on('mouseout', function(evt) {
+  $('.works-content').css('background', '#dea2c2f0');
+});
+
+$('.brand-g').on('mouseover', function(evt) {
+  $('.works-content').css('background', '#aed871f0');
+});
+$('.brand-g').on('mouseout', function(evt) {
+  $('.works-content').css('background', '#dea2c2f0');
+});
+
+$('.brand-h').on('mouseover', function(evt) {
+  $('.works-content').css('background', '#dace00f0');
+});
+$('.brand-h').on('mouseout', function(evt) {
+  $('.works-content').css('background', '#dea2c2f0');
+});
+
+$('.brand-i').on('mouseover', function(evt) {
+  $('.works-content').css('background', '#0f4a0ff0');
+});
+$('.brand-i').on('mouseout', function(evt) {
+  $('.works-content').css('background', '#dea2c2f0');
+});
+
+$('.brand-j').on('mouseover', function(evt) {
+  $('.works-content').css('background', '#1b0b31f0');
+});
+$('.brand-j').on('mouseout', function(evt) {
+  $('.works-content').css('background', '#dea2c2f0');
+});
+
+$('.brand-k').on('mouseover', function(evt) {
+  $('.works-content').css('background', '#8c1818ed');
+});
+$('.brand-k').on('mouseout', function(evt) {
+  $('.works-content').css('background', '#dea2c2f0');
+});
+
+$('.brand-l').on('mouseover', function(evt) {
+  $('.works-content').css('background', '#719dd8f0');
+});
+$('.brand-l').on('mouseout', function(evt) {
+  $('.works-content').css('background', '#dea2c2f0');
+});
